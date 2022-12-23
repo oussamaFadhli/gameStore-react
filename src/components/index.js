383116ccc455c79ebe17b79fa78e87ae8c5a25db
GorJani <www.oussama55aw5@gmail.com>
2022-12-23 16:11:52 +0100

    Game Store react js

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-export {default as Header} from './Header/Header'
+export {default as Header} from '../sections/Header/Header'
 export {default as Main} from './Main/Main'
-export {default as Footer} from './Footer/Footer'
 export {default as Hero} from './Hero/Hero'
+export {default as Footer} from '../sections/Footer/Footer'
--- src/sections/Header/Header.js
+++ src/sections/Header/Header.js
@@ -1,12 +1,12 @@
 import './Header.css'
-import NavItem,{NavItemDropDown} from '../NavItem/NavItem'
+import NavItem,{NavItemDropDown} from '../../components/NavItem/NavItem'
 
 const Header = (props) => {
   return (
     <div className="navbar navbar-expand-md navbar-dark cyborg-navbar">
         <div className="container">
-            <a href="#" className="navbar-brand">
-              <img src="https://castletowers.qicre.com/-/media/Intelligence-Bank/Town-Square/Retailers/goodgames-logo.png" alt ='store logo' width='150' height='70'/>
+            <a href="/" className="navbar-brand">
+              <img src="https://www.nutritechfit.com/wp-content/uploads/2020/04/game-logo.png" alt ='store logo' width='160' height='50'/>
             </a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainmenu">
                 <span className="navbar-toggler-icon"></span>
@@ -24,7 +24,7 @@ const Header = (props) => {
 
                 <NavItemDropDown>
                     <li className="nav-item dropdown">
-                        <a href="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Details</a>
+                        <a href="/" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Details</a>
                         <ul className="dropdown-menu">
                             <li><a href="#learn" className="dropdown-item">CSGO</a></li>
                             <li><a href="#next" className="dropdown-item">Warzone 2.0</a></li>
